f12b4ef0a2c7bd8faecf25c339abfc7c741c29c6
Rafael Cuevas <rafacuevas3@gmail.com>
2023-05-03 14:22:28 -0600

    Change API key

--- src/api.ts
+++ src/api.ts
@@ -47,7 +47,7 @@ api.get('/chat', async (req, res) => {
           headers: {
             'Content-Type': 'application/json',
             Authorization:
-              'Bearer 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'
+              'Bearer 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'
           }
         }
       )
